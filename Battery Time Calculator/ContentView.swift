13849fb0427683d59ea6589e308c42da8033aaa1
Jack Dellamea <jdellamea@lcs.on.ca>
2023-02-09 12:10:07 -0500

    Added some descriptions

--- Battery Time Calculator/ContentView.swift
+++ Battery Time Calculator/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        //Creating Layout of my App
+        //Molding the Base
         HStack {
             VStack {
+                //Creating Battery Text and Font
                 Text("(Insert Battery Here)%")
                     .font(.custom(
                         "AvenirNext-Bold",
                         fixedSize:33))
+                //Making the Text and checkbox for "Low Power Mode []"
                 HStack {
+                    
                     
                 }
             }
