3acd94b243860cf5fb9b705abb601b2ba3399e22
Jack Dellamea <jdellamea@lcs.on.ca>
2023-02-09 11:18:21 -0500

    Initial Commit

---
+++ Battery Time Calculator/Battery_Time_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Battery_Time_CalculatorApp.swift
+//  Battery Time Calculator
+//
+//  Created by Jack Dellamea on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Battery_Time_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
